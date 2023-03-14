551de549f010a58255af29b67a663887a52dd6c1
BienkowskiTomasz <it.bienkowski@gmail.com>
2023-03-14 13:04:23 +0100

    Initial commit

---
+++ src/main/java/com/yobombel/beersmithstats/BeersmithStatsApplication.java
@@ -0,0 +1,13 @@
+package com.yobombel.beersmithstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersmithStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeersmithStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yobombel/beersmithstats/BeersmithStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yobombel.beersmithstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeersmithStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
